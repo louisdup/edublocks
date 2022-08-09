1c27b2ec3863cc6bccdb2e130a3413b9feda1793
Joshua Lowe <joshualowe1002@gmail.com>
2022-08-09 23:48:28 +0100

    Remove references to input component

--- src/components/constants.ts
+++ src/components/constants.ts
@@ -4,7 +4,6 @@
 export enum ComponentName {
 	EbSidebar = "EbSidebar",
 	EbIcon = "EbIcon",
-	EbInput = "EbInput",
 	EbHeading = "EbHeading",
 	EbButton = "EbButton",
 	EbBottombar = "EbBottombar"
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,7 +3,6 @@ import { ComponentName } from "./constants";
 
 import EbSidebar from "./eb-sidebar/eb-sidebar.vue";
 import EbIcon from "./eb-icon/eb-icon.vue";
-import EbInput from "./eb-input/eb-input.vue";
 import EbHeading from "./eb-heading/eb-heading.vue";
 import EbButton from "./eb-button/eb-button.vue";
 import EbBottombar from "./eb-bottombar/eb-bottombar.vue";
@@ -15,7 +14,6 @@ export default {
 	install(app: App): void {
 		app.component(ComponentName.EbSidebar, EbSidebar);
 		app.component(ComponentName.EbIcon, EbIcon);
-		app.component(ComponentName.EbInput, EbInput);
 		app.component(ComponentName.EbHeading, EbHeading);
 		app.component(ComponentName.EbButton, EbButton);
 		app.component(ComponentName.EbBottombar, EbBottombar);
