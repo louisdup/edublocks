ebaf926b142b3d6653276818de2e501608b88bd3
Joshua Lowe <joshualowe1002@gmail.com>
2022-08-21 20:31:01 +0100

    Add iOS and Android capacitor apps

---
+++ android/app/src/main/java/com/joshualowe/edublocks/MainActivity.java
@@ -0,0 +1,5 @@
+package com.joshualowe.edublocks;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.joshualowe.edublocks',
+  appName: 'EduBlocks',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
