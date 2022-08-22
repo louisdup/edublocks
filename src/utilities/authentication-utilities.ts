2eaf138b1da81f396c446d332597ba7f273050a9
Joshua Lowe <joshualowe1002@gmail.com>
2022-08-22 21:43:48 +0100

    Update signin function to use namespaced firebase function

--- src/utilities/authentication-utilities.ts
+++ src/utilities/authentication-utilities.ts
@@ -1,4 +1,5 @@
-import { getAuth, Auth, User, signInWithEmailAndPassword, UserCredential, AuthError } from "firebase/auth";
+import { AuthError } from "@firebase/auth";
+import firebase from "firebase/compat/app";
 import { ref, Ref } from "vue";
 
 /**
@@ -8,21 +9,21 @@ export class AuthenticationUtilities {
 	/**
 	 * Returns firebase auth instance.
 	 */
-	public static auth(): Auth {
-		return getAuth();
+	public static auth(): firebase.auth.Auth {
+		return firebase.auth();
 	}
 
 	/**
 	 * Returns current user instance.
 	 */
-	public static currentUser: Ref<User | undefined> = ref();
+	public static currentUser: Ref<firebase.User | null> = ref(null);
 
 	/**
 	 * Login with an email and password, then set the current user instance.
 	 */
 	public static async loginWithEmailAndPassword(email: string, password: string): Promise<void | AuthError> {
-		return signInWithEmailAndPassword(this.auth(), email, password).then((userCredential: UserCredential) => {
-			this.currentUser.value = userCredential.user;
+		return this.auth().signInWithEmailAndPassword(email, password).then((user: firebase.auth.UserCredential) => {
+			this.currentUser.value = user.user;
 		}).catch((error: AuthError) => {
 			throw error;
 		});
