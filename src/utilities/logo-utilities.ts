f5df77931ad7ad701c92b82cf88af3bebfd276f5
Joshua Lowe <joshualowe1002@gmail.com>
2022-09-05 15:25:47 +0100

    Update logo paths in logo utilities file

--- src/utilities/logo-utilities.ts
+++ src/utilities/logo-utilities.ts
@@ -6,13 +6,13 @@ export class LogoUtilities {
 	 * Returns the full logo path.
 	 */
 	public static getFullLogoPath(): string {
-		return "/images/logo.svg";
+		return "/images/logo/logo.svg";
 	}
 
 	/**
 	 * Returns the mini logo path.
 	 */
 	public static getMiniLogoPath(): string {
-		return "/images/logo-mini.svg";
+		return "/images/logo/logo-mini.svg";
 	}
 }
