91caab425fe19d5bbe639d0fadbb7e7b37564eb6
Joshua Lowe <joshualowe1002@gmail.com>
2022-11-01 21:09:32 +0000

    Add common basic blocks definitions

---
+++ src/modes/common/blocks/python/basic/definitions.ts
@@ -0,0 +1,567 @@
+const basicColor: string = "#E71D64";
+
+export default function getDefinitions(): void {
+	Blockly.Blocks["print"] = {
+		init: function(): void {
+			this.appendDummyInput()
+				.appendField("print(");
+			this.appendValueInput("parameters")
+				.setCheck(null);
+			this.appendDummyInput()
+				.appendField(")");
+			this.setInputsInline(true);
+			this.setPreviousStatement(true, null);
+			this.setNextStatement(true, null);
+			this.setColour(basicColor);
+		}
+	};
+
+	Blockly.Blocks["input"] = {
+		init: function(): void {
+			this.appendDummyInput()
+				.appendField("input(");
+			this.appendValueInput("parameters")
+				.setCheck(null);
+			this.appendDummyInput()
+				.appendField(")");
+			this.setInputsInline(true);
+			this.setOutput(true, null);
+			this.setColour(basicColor);
+		}
+	};
+
+	Blockly.Blocks["time_sleep"] = {
+		init: function(): void {
+			this.appendDummyInput()
+				.appendField("time.sleep(");
+			this.appendValueInput("parameters")
+				.setCheck(null);
+			this.appendDummyInput()
+				.appendField(")");
+			this.setInputsInline(true);
+			this.setPreviousStatement(true, null);
+			this.setNextStatement(true, null);
+			this.setColour(basicColor);
+		}
+	};
+
+	Blockly.Blocks["convert_to_string"] = {
+		init: function(): void {
+			this.appendDummyInput()
+				.appendField("str(");
+			this.appendValueInput("parameters")
+				.setCheck(null);
+			this.appendDummyInput()
+				.appendField(")");
+			this.setInputsInline(true);
+			this.setOutput(true, null);
+			this.setColour(basicColor);
+		}
+	};
+
+	Blockly.Blocks["convert_to_integer"] = {
+		init: function(): void {
+			this.appendDummyInput()
+				.appendField("int(");
+			this.appendValueInput("parameters")
+				.setCheck(null);
+			this.appendDummyInput()
+				.appendField(")");
+			this.setInputsInline(true);
+			this.setOutput(true, null);
+			this.setColour(basicColor);
+		}
+	};
+
+	Blockly.Blocks["convert_to_float"] = {
+		init: function(): void {
+			this.appendDummyInput()
+				.appendField("float(");
+			this.appendValueInput("parameters")
+				.setCheck(null);
+			this.appendDummyInput()
+				.appendField(")");
+			this.setInputsInline(true);
+			this.setOutput(true, null);
+			this.setColour(basicColor);
+		}
+	};
+
+	Blockly.Blocks["global"] = {
+		init: function(): void {
+			this.appendDummyInput()
+				.appendField("global ");
+			this.appendValueInput("value")
+				.setCheck(null);
+			this.setInputsInline(true);
+			this.setPreviousStatement(true, null);
+			this.setNextStatement(true, null);
+			this.setColour(basicColor);
+		}
+	};
+
+	Blockly.Blocks["length"] = {
+		init: function(): void {
+			this.appendDummyInput()
+				.appendField("len(");
+			this.appendValueInput("parameters")
+				.setCheck(null);
+			this.appendDummyInput()
+				.appendField(")");
+			this.setInputsInline(true);
+			this.setOutput(true, null);
+			this.setColour(basicColor);
+		}
+	};
+
+	Blockly.Blocks["case"] = {
+		init: function(): void {
+			this.appendValueInput("value")
+				.setCheck(null);
+			this.appendDummyInput()
+				.appendField(".")
+				.appendField(new Blockly.FieldDropdown([["upper","upper"], ["lower","lower"], ["title","title"]]), "case")
+				.appendField("()");
+			this.setInputsInline(true);
+			this.setOutput(true, null);
+			this.setColour(basicColor);
+		}
+	};
+
+	Blockly.Blocks["reverse"] = {
+		init: function(): void {
+			this.appendValueInput("value")
+				.setCheck(null);
+			this.appendDummyInput()
+				.appendField("[::-1]");
+			this.setInputsInline(true);
+			this.setOutput(true, null);
+			this.setColour(basicColor);
+		}
+	};
+
+	Blockly.Blocks["replace"] = {
+		init: function(): void {
+			this.appendValueInput("value")
+				.setCheck(null);
+			this.appendDummyInput()
+				.appendField(".replace(");
+			this.appendValueInput("parameters")
+				.setCheck(null);
+			this.appendDummyInput()
+				.appendField(")");
+			this.setInputsInline(true);
+			this.setOutput(true, null);
+			this.setColour(basicColor);
+		}
+	};
+
+	Blockly.Blocks["count"] = {
+		init: function(): void {
+			this.appendValueInput("value")
+				.setCheck(null);
+			this.appendDummyInput()
+				.appendField(".count(");
+			this.appendValueInput("parameters")
+				.setCheck(null);
+			this.appendDummyInput()
+				.appendField(")");
+			this.setInputsInline(true);
+			this.setOutput(true, null);
+			this.setColour(basicColor);
+		}
+	};
+
+	Blockly.Blocks["strip"] = {
+		init: function(): void {
+			this.appendValueInput("value")
+				.setCheck(null);
+			this.appendDummyInput()
+				.appendField(".")
+				.appendField(new Blockly.FieldDropdown([["strip","strip"], ["lstrip","lstrip"], ["rstrip","rsrtip"]]), "strip_type")
+				.appendField("()");
+			this.setInputsInline(true);
+			this.setOutput(true, null);
+			this.setColour(basicColor);
+		}
+	};
+
+	Blockly.Blocks["find"] = {
+		init: function(): void {
+			this.appendValueInput("value")
+				.setCheck(null);
+			this.appendDummyInput()
+				.appendField(".")
+				.appendField(new Blockly.FieldDropdown([["find","find"], ["rfind","rfind"]]), "find_type")
+				.appendField("(");
+			this.appendValueInput("parameters")
+				.setCheck(null);
+			this.appendDummyInput()
+				.appendField(")");
+			this.setInputsInline(true);
+			this.setOutput(true, null);
+			this.setColour(basicColor);
+		}
+	};
+	
+	Blockly.Blocks["text_at_index"] = {
+		init: function(): void {
+			this.appendValueInput("value")
+				.setCheck(null);
+			this.appendDummyInput()
+				.appendField("[");
+			this.appendValueInput("index")
+				.setCheck(null);
+			this.appendDummyInput()
+				.appendField("]");
+			this.setInputsInline(true);
+			this.setOutput(true, null);
+			this.setColour(basicColor);
+		}
+	};
+
+	Blockly.Blocks["arithmetic"] = {
+		init: function(): void {
+			this.appendValueInput("value_1")
+				.setCheck(null);
+			this.appendDummyInput()
+				.appendField(new Blockly.FieldDropdown([["+","+"], ["-","-"], ["/","/"], ["%","%"], ["*","*"], ["**","**"], ["//","//"]]), "operator");
+			this.appendValueInput("value_2")
+				.setCheck(null);
+			this.setInputsInline(true);
+			this.setOutput(true);
+			this.setColour(basicColor);
+		}
+	};
+  
+	Blockly.Blocks["round"] = {
+		init: function(): void {
+			this.appendDummyInput()
+				.appendField("round(");
+			this.appendValueInput("parameters")
+				.setCheck(null);
+			this.appendDummyInput()
+				.appendField(")");
+			this.setInputsInline(true);
+			this.setOutput(true, null);
+			this.setColour(basicColor);
+		}
+	};
+
+	Blockly.Blocks["if"] = {
+		init: function(): void {
+			this.appendDummyInput()
+				.appendField("if ");
+			this.appendValueInput("condition")
+				.setCheck(null);
+			this.appendDummyInput()
+				.appendField(":");
+			this.appendStatementInput("value")
+				.setCheck(null);
+			this.setInputsInline(true);
+			this.setPreviousStatement(true, null);
+			this.setNextStatement(true, null);
+			this.setColour(basicColor);
+		}
+	};
+
+	Blockly.Blocks["elif"] = {
+		init: function(): void {
+			this.appendDummyInput()
+				.appendField("elif ");
+			this.appendValueInput("condition")
+				.setCheck(null);
+			this.appendDummyInput()
+				.appendField(":");
+			this.appendStatementInput("value")
+				.setCheck(null);
+			this.setInputsInline(true);
+			this.setPreviousStatement(true, null);
+			this.setNextStatement(true, null);
+			this.setColour(basicColor);
+		}
+	};
+
+	Blockly.Blocks["else"] = {
+		init: function(): void {
+			this.appendDummyInput()
+				.appendField("else:");
+			this.appendStatementInput("value")
+				.setCheck(null);
+			this.setInputsInline(true);
+			this.setPreviousStatement(true, null);
+			this.setNextStatement(true, null);
+			this.setColour(basicColor);
+		}
+	};
+  
+	Blockly.Blocks["compare"] = {
+		init: function(): void {
+			this.appendValueInput("value_1")
+				.setCheck(null);
+			this.appendDummyInput()
+				.appendField(new Blockly.FieldDropdown([["==","=="], ["!=","!="], ["is", "is"], ["is not","is not"], ["<","<"], ["<=","<="], [">",">"], [">=",">="]]), "operator");
+			this.appendValueInput("value_2")
+				.setCheck(null);
+			this.setInputsInline(true);
+			this.setOutput(true, null);
+			this.setColour(basicColor);
+		}
+	};
+
+	Blockly.Blocks["and_or"] = {
+		init: function(): void {
+			this.appendValueInput("value_1")
+				.setCheck(null);
+			this.appendDummyInput()
+				.appendField(new Blockly.FieldDropdown([["and","and"], ["or","or"]]), "operator");
+			this.appendValueInput("value_2")
+				.setCheck(null);
+			this.setInputsInline(true);
+			this.setOutput(true, null);
+			this.setColour(basicColor);
+		}
+	};
+
+	Blockly.Blocks["not"] = {
+		init: function(): void {
+			this.appendDummyInput()
+				.appendField("not ");
+			this.appendValueInput("value")
+				.setCheck(null);
+			this.setInputsInline(true);
+			this.setOutput(true, null);
+			this.setColour(basicColor);
+		}
+	};
+
+	Blockly.Blocks["list"] = {
+		init: function(): void {
+			this.appendDummyInput()
+				.appendField("[");
+			this.appendValueInput("data")
+				.setCheck(null);
+			this.appendDummyInput()
+				.appendField("]");
+			this.setInputsInline(true);
+			this.setOutput(true, null);
+			this.setColour(basicColor);
+		}
+	};
+
+	Blockly.Blocks["list_get_item_at_index"] = {
+		init: function(): void {
+			this.appendDummyInput()
+				.appendField(new Blockly.FieldVariable("variable_name"), "variable_name")
+				.appendField("[");
+			this.appendValueInput("parameters")
+				.setCheck(null);
+			this.appendDummyInput()
+				.appendField("]");
+			this.setInputsInline(true);
+			this.setOutput(true, null);
+			this.setColour(basicColor);
+		}
+	};
+  
+	Blockly.Blocks["list_reverse"] = {
+		init: function(): void {
+			this.appendDummyInput()
+				.appendField(new Blockly.FieldVariable("variable_name"), "variable_name")
+				.appendField(".reverse()");
+			this.setPreviousStatement(true, null);
+			this.setNextStatement(true, null);
+			this.setColour(basicColor);
+		}
+	};
+
+	Blockly.Blocks["list_find_index"] = {
+		init: function(): void {
+			this.appendDummyInput()
+				.appendField(new Blockly.FieldVariable("variable_name"), "variable_name")
+				.appendField(".index(");
+			this.appendValueInput("parameters")
+				.setCheck(null);
+			this.appendDummyInput()
+				.appendField(")");
+			this.setInputsInline(true);
+			this.setOutput(true, null);
+			this.setColour(basicColor);
+		}
+	};
+
+	Blockly.Blocks["list_append"] = {
+		init: function(): void {
+			this.appendDummyInput()
+				.appendField(new Blockly.FieldVariable("variable_name"), "variable_name")
+				.appendField(".append(");
+			this.appendValueInput("parameters")
+				.setCheck(null);
+			this.appendDummyInput()
+				.appendField(")");
+			this.setInputsInline(true);
+			this.setPreviousStatement(true, null);
+			this.setNextStatement(true, null);
+			this.setColour(basicColor);
+		}
+	};
+
+	Blockly.Blocks["while"] = {
+		init: function(): void {
+			this.appendDummyInput()
+				.appendField("while ");
+			this.appendValueInput("condition")
+				.setCheck(null);
+			this.appendDummyInput()
+				.appendField(":");
+			this.appendStatementInput("value")
+				.setCheck(null);
+			this.setInputsInline(true);
+			this.setPreviousStatement(true, null);
+			this.setNextStatement(true, null);
+			this.setColour(basicColor);
+		}
+	};
+
+	Blockly.Blocks["for_range"] = {
+		init: function(): void {
+			this.appendDummyInput()
+				.appendField("for ")
+				.appendField(new Blockly.FieldVariable("i"), "variable")
+				.appendField(" in range(");
+			this.appendValueInput("parameters")
+				.setCheck(null);
+			this.appendDummyInput()
+				.appendField("):");
+			this.appendStatementInput("value")
+				.setCheck(null);
+			this.setInputsInline(true);
+			this.setPreviousStatement(true, null);
+			this.setNextStatement(true, null);
+			this.setColour(basicColor);
+		}
+	};
+
+	Blockly.Blocks["for"] = {
+		init: function(): void {
+			this.appendDummyInput()
+				.appendField("for ")
+				.appendField(new Blockly.FieldVariable("i"), "variable")
+				.appendField(" in ");
+			this.appendValueInput("condition")
+				.setCheck(null);
+			this.appendDummyInput()
+				.appendField(":");
+			this.appendStatementInput("value")
+				.setCheck(null);
+			this.setInputsInline(true);
+			this.setPreviousStatement(true, null);
+			this.setNextStatement(true, null);
+			this.setColour(basicColor);
+		}
+	};
+  
+	Blockly.Blocks["break"] = {
+		init: function(): void {
+			this.appendDummyInput()
+				.appendField("break");
+			this.setInputsInline(true);
+			this.setPreviousStatement(true, null);
+			this.setColour(basicColor);
+		}
+	};
+
+	Blockly.Blocks["define_function"] = {
+		init: function(): void {
+			this.appendDummyInput()
+				.appendField("def ")
+				.appendField(new Blockly.FieldVariable("function_name"), "function_name")
+				.appendField("(");
+			this.appendValueInput("parameters")
+				.setCheck(null);
+			this.appendDummyInput()
+				.appendField("):");
+			this.appendStatementInput("value")
+				.setCheck(null);
+			this.setInputsInline(true);
+			this.setPreviousStatement(true, null);
+			this.setNextStatement(true, null);
+			this.setColour(basicColor);
+		}
+	};
+
+	Blockly.Blocks["define_function"] = {
+		init: function(): void {
+			this.appendDummyInput()
+				.appendField("def ")
+				.appendField(new Blockly.FieldVariable("function_name"), "function_name")
+				.appendField("(");
+			this.appendValueInput("parameters")
+				.setCheck(null);
+			this.appendDummyInput()
+				.appendField("):");
+			this.appendStatementInput("value")
+				.setCheck(null);
+			this.setInputsInline(true);
+			this.setPreviousStatement(true, null);
+			this.setNextStatement(true, null);
+			this.setColour(basicColor);
+		}
+	};
+
+	Blockly.Blocks["define_class"] = {
+		init: function(): void {
+			this.appendDummyInput()
+				.appendField("class ")
+				.appendField(new Blockly.FieldVariable("class_name"), "class_name")
+				.appendField(":");
+			this.appendStatementInput("value")
+				.setCheck(null);
+			this.setInputsInline(true);
+			this.setPreviousStatement(true, null);
+			this.setNextStatement(true, null);
+			this.setColour(basicColor);
+		}
+	};
+
+	Blockly.Blocks["return_function"] = {
+		init: function(): void {
+			this.appendDummyInput()
+				.appendField("return ")
+				.appendField(new Blockly.FieldVariable("function_name"), "function_name");
+			this.setPreviousStatement(true, null);
+			this.setColour(basicColor);
+		}
+	};
+
+	Blockly.Blocks["call_function"] = {
+		init: function(): void {
+			this.appendDummyInput()
+				.appendField(new Blockly.FieldVariable("function_name"), "function_name")
+				.appendField("(");
+			this.appendValueInput("parameters")
+				.setCheck(null);
+			this.appendDummyInput()
+				.appendField(")");
+			this.setInputsInline(true);
+			this.setPreviousStatement(true, null);
+			this.setNextStatement(true, null);
+			this.setColour(basicColor);
+		}
+	};
+	
+	Blockly.Blocks["class_self"] = {
+		init: function(): void {
+			this.appendDummyInput()
+				.appendField("self.")
+				.appendField(new Blockly.FieldVariable("i"), "variable_name")
+				.appendField(new Blockly.FieldDropdown([["=","="], ["+=","+="], ["-=","-="]]), "operator");
+			this.appendValueInput("value")
+				.setCheck(null);
+			this.setInputsInline(true);
+			this.setPreviousStatement(true, null);
+			this.setNextStatement(true, null);
+			this.setColour(basicColor);
+		}
+	};	
+}
---
+++ src/modes/common/blocks/python/basic/generators.ts
@@ -0,0 +1,259 @@
+export default function getGenerators(): void {
+	Blockly.Python["print"] = function(block: Blockly.BlockSvg): string {
+		const parameters: string = Blockly.Python.valueToCode(block, "parameters", 0);
+		const code: string = `print(${parameters})\n`;
+		return code;
+	};
+
+	Blockly.Python["input"] = function(block: Blockly.BlockSvg): Array<string | number> {
+		const parameters: string = Blockly.Python.valueToCode(block, "parameters", 0);
+		const code: string = `input(${parameters})`;
+		return [code, 0];
+	};
+
+	Blockly.Python["time_sleep"] = function(block: Blockly.BlockSvg): string {
+		const parameters: string = Blockly.Python.valueToCode(block, "parameters", 0);
+		const code: string = `time.sleep(${parameters})\n`;
+		return code;
+	};
+
+	Blockly.Python["convert_to_string"] = function(block: Blockly.BlockSvg): Array<string | number> {
+		const parameters: string = Blockly.Python.valueToCode(block, "parameters", 0);
+		const code: string = `str(${parameters})`;
+		return [code, 0];
+	};
+
+	Blockly.Python["convert_to_integer"] = function(block: Blockly.BlockSvg): Array<string | number> {
+		const parameters: string = Blockly.Python.valueToCode(block, "parameters", 0);
+		const code: string = `int(${parameters})`;
+		return [code, 0];
+	};
+
+	Blockly.Python["convert_to_float"] = function(block: Blockly.BlockSvg): Array<string | number> {
+		const parameters: string = Blockly.Python.valueToCode(block, "parameters", 0);
+		const code: string = `float(${parameters})`;
+		return [code, 0];
+	};
+
+	Blockly.Python["global"] = function(block: Blockly.BlockSvg): string {
+		const value: string = Blockly.Python.valueToCode(block, "value", 0);
+		const code: string = `global ${value}\n`;
+		return code;
+	};
+
+	Blockly.Python["length"] = function(block: Blockly.BlockSvg): Array<string | number> {
+		const parameters: string = Blockly.Python.valueToCode(block, "parameters", 0);
+		const code: string = `len(${parameters})`;
+		return [code, 0];
+	};
+
+	Blockly.Python["case"] = function(block: Blockly.BlockSvg): Array<string | number> {
+		const value: string = Blockly.Python.valueToCode(block, "value", 0);
+		const textCase: string = block.getFieldValue("case");
+		const code: string = `${value}.${textCase}()`;
+		return [code, 0];
+	};
+
+	Blockly.Python["reverse"] = function(block: Blockly.BlockSvg): Array<string | number> {
+		const value: string = Blockly.Python.valueToCode(block, "value", 0);
+		const code: string = `${value}[::-1]`;
+		return [code, 0];
+	};
+
+	Blockly.Python["replace"] = function(block: Blockly.BlockSvg): Array<string | number> {
+		const value: string = Blockly.Python.valueToCode(block, "value", 0);
+		const parameters: string = Blockly.Python.valueToCode(block, "parameters", 0);
+		const code: string = `${value}.replace(${parameters})`;
+		return [code, 0];
+	};
+
+	Blockly.Python["count"] = function(block: Blockly.BlockSvg): Array<string | number> {
+		const value: string = Blockly.Python.valueToCode(block, "value", 0);
+		const parameters: string = Blockly.Python.valueToCode(block, "suparametersbstring", 0);
+		const code: string = `${value}.count(${parameters})`;
+		return [code, 0];
+	};
+
+	Blockly.Python["strip"] = function(block: Blockly.BlockSvg): Array<string | number> {
+		const value: string = Blockly.Python.valueToCode(block, "value", 0);
+		const stripType: string = block.getFieldValue("strip_type");
+		const code: string = `${value}.${stripType}()`;
+		return [code, 0];
+	};
+
+	Blockly.Python["find"] = function(block: Blockly.BlockSvg): Array<string | number> {
+		const value: string = Blockly.Python.valueToCode(block, "value", 0);
+		const findType: string = block.getFieldValue("find_type");
+		const parameters: string = Blockly.Python.valueToCode(block, "parameters", 0);
+		const code: string = `${value}.${findType}(${parameters})`;
+		return [code, 0];
+	};
+
+	Blockly.Python["text_at_index"] = function(block: Blockly.BlockSvg): Array<string | number> {
+		const value: string = Blockly.Python.valueToCode(block, "value", 0);
+		const index: string = Blockly.Python.valueToCode(block, "index", 0);
+		const code: string = `${value}[${index}]`;
+		return [code, 0];
+	};
+
+	Blockly.Python["arithmetic"] = function(block: Blockly.BlockSvg): Array<string | number> {
+		const value1: string = Blockly.Python.valueToCode(block, "value_1", 0);
+		const operator: string = block.getFieldValue("operator");
+		const value2: string = Blockly.Python.valueToCode(block, "value_2", 0);
+		const code: string = `${value1} ${operator} ${value2}`;
+		return [code, 0];
+	};
+
+	Blockly.Python["round"] = function(block: Blockly.BlockSvg): Array<string | number> {
+		const parameters: string = Blockly.Python.valueToCode(block, "parameters", 0);
+		const code: string = `round(${parameters})`;
+		return [code, 0];
+	};
+
+	Blockly.Python["if"] = function(block: Blockly.BlockSvg): string {
+		const condition: string = Blockly.Python.valueToCode(block, "condition", 0);
+		let value: string = Blockly.Python.statementToCode(block, "value");
+		value = Blockly.Python.addLoopTrap(value, block) || Blockly.Python.PASS;
+		const code: string = `if ${condition}:\n${value}`;
+		return code;
+	};
+
+	Blockly.Python["elif"] = function(block: Blockly.BlockSvg): string {
+		const condition: string = Blockly.Python.valueToCode(block, "condition", 0);
+		let value: string = Blockly.Python.statementToCode(block, "value");
+		value = Blockly.Python.addLoopTrap(value, block) || Blockly.Python.PASS;
+		const code: string = `elif ${condition}:\n${value}`;
+		return code;
+	};
+
+	Blockly.Python["else"] = function(block: Blockly.BlockSvg): string {
+		let value: string = Blockly.Python.statementToCode(block, "value");
+		value = Blockly.Python.addLoopTrap(value, block) || Blockly.Python.PASS;
+		const code: string = `else:\n${value}`;
+		return code;
+	};
+
+	Blockly.Python["compare"] = function(block: Blockly.BlockSvg): Array<string | number> {
+		const value1: string = Blockly.Python.valueToCode(block, "value_1", 0);
+		const operator: string = block.getFieldValue("operator");
+		const value2: string = Blockly.Python.valueToCode(block, "value_2", 0);
+		const code: string = `${value1} ${operator} ${value2}`;
+		return [code, 0];
+	};
+
+	Blockly.Python["and_or"] = function(block: Blockly.BlockSvg): Array<string | number> {
+		const value1: string = Blockly.Python.valueToCode(block, "value_1", 0);
+		const operator: string = block.getFieldValue("operator");
+		const value2: string = Blockly.Python.valueToCode(block, "value_2", 0);
+		const code: string = `${value1} ${operator} ${value2}`;
+		return [code, 0];
+	};
+
+	Blockly.Python["not"] = function(block: Blockly.BlockSvg): Array<string | number> {
+		const value: string = Blockly.Python.valueToCode(block, "value", 0);
+		const code: string = `not ${value}`;
+		return [code, 0];
+	};
+
+	Blockly.Python["list"] = function(block: Blockly.BlockSvg): Array<string> {
+		const data: string = Blockly.Python.valueToCode(block, "data", 0);
+		const code: string = `[${data}]`;
+		return [code, Blockly.Python.ORDER_ATOMIC];
+	};
+
+	Blockly.Python["list_get_item_at_index"] = function(block: Blockly.BlockSvg): Array<string> {
+		const variableName: string = Blockly.Python.nameDB_.getName(block.getFieldValue("variable_name"), Blockly.VARIABLE_CATEGORY_NAME);
+		const parameters: string = Blockly.Python.valueToCode(block, "parameters", 0);
+		const code: string = `${variableName}[${parameters}]`;
+		return [code, Blockly.Python.ORDER_ATOMIC];
+	};
+
+	Blockly.Python["list_reverse"] = function(block: Blockly.BlockSvg): string {
+		const variableName: string = Blockly.Python.nameDB_.getName(block.getFieldValue("variable_name"), Blockly.VARIABLE_CATEGORY_NAME);
+		const code: string = `${variableName}.reverse()\n`;
+		return code;
+	};
+
+	Blockly.Python["list_find_index"] = function(block: Blockly.BlockSvg): Array<string> {
+		const variableName: string = Blockly.Python.nameDB_.getName(block.getFieldValue("variable_name"), Blockly.VARIABLE_CATEGORY_NAME);
+		const parameters: string = Blockly.Python.valueToCode(block, "parameters", 0);
+		const code: string = `${variableName}.index(${parameters})`;
+		return [code, Blockly.Python.ORDER_ATOMIC];
+	};
+
+	Blockly.Python["list_append"] = function(block: Blockly.BlockSvg): string {
+		const variableName: string = Blockly.Python.nameDB_.getName(block.getFieldValue("variable_name"), Blockly.VARIABLE_CATEGORY_NAME);
+		const parameters: string = Blockly.Python.valueToCode(block, "parameters", 0);
+		const code: string = `${variableName}.append(${parameters})\n`;
+		return code;
+	};
+
+	Blockly.Python["while"] = function(block: Blockly.BlockSvg): string {
+		const condition: string = Blockly.Python.valueToCode(block, "condition", 0);
+		let value: string = Blockly.Python.statementToCode(block, "value");
+		value = Blockly.Python.addLoopTrap(value, block) || Blockly.Python.PASS;
+		const code: string = `while ${condition}:\n${value}`;
+		return code;
+	};
+
+	Blockly.Python["for_range"] = function(block: Blockly.BlockSvg): string {
+		const variable: string = Blockly.Python.nameDB_.getName(block.getFieldValue("variable"), Blockly.VARIABLE_CATEGORY_NAME);
+		const parameters: string = Blockly.Python.valueToCode(block, "parameters", 0);
+		let value: string = Blockly.Python.statementToCode(block, "value");
+		value = Blockly.Python.addLoopTrap(value, block) || Blockly.Python.PASS;
+		const code: string = `for ${variable} in range(${parameters}):\n${value}`;
+		return code;
+	};
+
+	Blockly.Python["for"] = function(block: Blockly.BlockSvg): string {
+		const variable: string = Blockly.Python.nameDB_.getName(block.getFieldValue("variable"), Blockly.VARIABLE_CATEGORY_NAME);
+		const condition: string = Blockly.Python.valueToCode(block, "condition", 0);
+		let value: string = Blockly.Python.statementToCode(block, "value");
+		value = Blockly.Python.addLoopTrap(value, block) || Blockly.Python.PASS;
+		const code: string = `for ${variable} in ${condition}:\n${value}`;
+		return code;
+	};
+  
+	Blockly.Python["break"] = function(): string {
+		const code: string = "break\n";
+		return code;
+	};
+
+	Blockly.Python["define_function"] = function(block: Blockly.BlockSvg): string {
+		const functionName: string = Blockly.Python.nameDB_.getName(block.getFieldValue("function_name"), Blockly.VARIABLE_CATEGORY_NAME);
+		const parameters: string = Blockly.Python.valueToCode(block, "parameters", 0);
+		let value: string = Blockly.Python.statementToCode(block, "value");
+		value = Blockly.Python.addLoopTrap(value, block) || Blockly.Python.PASS;
+		const code: string = `def ${functionName}(${parameters}):\n${value}`;
+		return code;
+	};
+
+	Blockly.Python["define_class"] = function(block: Blockly.BlockSvg): string {
+		const className: string = Blockly.Python.nameDB_.getName(block.getFieldValue("class_name"), Blockly.VARIABLE_CATEGORY_NAME);
+		let value: string = Blockly.Python.statementToCode(block, "value");
+		value = Blockly.Python.addLoopTrap(value, block) || Blockly.Python.PASS;
+		const code: string = `class ${className}:\n${value}`;
+		return code;
+	};
+
+	Blockly.Python["return_function"] = function(block: Blockly.BlockSvg): string {
+		const functionName: string = Blockly.Python.nameDB_.getName(block.getFieldValue("function_name"), Blockly.VARIABLE_CATEGORY_NAME);
+		const code: string = `return ${functionName}\n`;
+		return code;
+	};
+
+	Blockly.Python["call_function"] = function(block: Blockly.BlockSvg): string {
+		const functionName: string = Blockly.Python.nameDB_.getName(block.getFieldValue("function_name"), Blockly.VARIABLE_CATEGORY_NAME);
+		const parameters: string = Blockly.Python.valueToCode(block, "parameters", 0);
+		const code: string = `${functionName}(${parameters})\n`;
+		return code;
+	};
+
+	Blockly.Python["class_self"] = function(block: Blockly.BlockSvg): string {
+		const variableName: string = Blockly.Python.nameDB_.getName(block.getFieldValue("variable_name"), Blockly.VARIABLE_CATEGORY_NAME);
+		const operator: string = block.getFieldValue("operator");
+		const value: string = Blockly.Python.valueToCode(block, "value", 0);
+		const code: string = `self.${variableName} ${operator} ${value}\n`;
+		return code;
+	};
+}
