0044bf42ac2c287c5bb8345a5837049688d05d21
Joshua Lowe <joshualowe1002@gmail.com>
2022-11-12 23:09:32 +0000

    Add projects list to mobile

--- src/layouts/mobile-layout/mobile-layout.vue
+++ src/layouts/mobile-layout/mobile-layout.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="h-screen w-screen flex flex-col bg-gray-100 overflow-hidden">
     <mobile-header :title="title" />
-    <div class="h-full w-full p-4 space-y-5 overflow-y-auto">
+    <div
+      class="h-full w-full p-4 space-y-5 overflow-y-auto"
+      @scroll="$emit('scroll', $event)"
+    >
       <slot />
     </div>
     <mobile-nav />
   </div>
+
+  <!-- Modal Dynamic Component -->
+  <transition
+    enter-active-class="transition duration-300 ease-out"
+    enter-from-class="opacity-0"
+    enter-to-class="opacity-100"
+    leave-active-class="transition duration-150 ease-in"
+    leave-from-class="opacity-100"
+    leave-to-class="opacity-0"
+  >
+    <component
+      :is="state.modal?.modal"
+      v-bind="state.modal?.options"
+    />
+  </transition>
 </template>
 
 <script setup lang="ts">
 import MobileNav from "./components/mobile-nav/mobile-nav.vue";
 import MobileHeader from "./components/mobile-header/mobile-header.vue";
 
+import { state } from "@/data/state";
+
 defineProps<{
 	title?: string;
 }>();
+
+defineEmits(["scroll"]);
 </script>
--- src/views/projects/templates/projects-mobile.vue
+++ src/views/projects/templates/projects-mobile.vue
@@ -1,8 +1,31 @@
 <template>
-  <mobile-layout>
-    <!-- Title bar for projects page. -->
-    <eb-heading :label="view.getPageTitle()" />
-    <!--  -->
+  <mobile-layout
+    :title="view.getPageTitle()"
+    @scroll="view.onPageScroll($event)"
+  >
+    <!-- Search box -->
+    <eb-h-stack :spacing="4">
+      <eb-search
+        v-model="view.state.search"
+        :placeholder="view.getText('search-projects')"
+        @input="view.onSearchInput()"
+      />
+
+      <eb-button
+        :icon="['far', 'file-import']"
+        color="white"
+        icon-color="darkGray"
+        @click="view.onOpenLocalProjectClicked()"
+      />
+    </eb-h-stack>
+
+    <!-- Projects Table -->
+    <eb-table
+      :items="view.getProjects()"
+      :is-loading="view.isProjectsTableLoading()"
+      :empty-state-title="view.getProjectsTableEmptyStateTitle()"
+      :empty-state-subtitle="view.getProjectsTableEmptyStateSubtitle()"
+    />
   </mobile-layout>
 </template>
 
